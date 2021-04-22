5272ed2d0d7c18cc5aad452382730ad8bbad62a6
Do Nhu Vy <donhuvy@hotmail.com>
2021-04-23 00:00:25 +0700

    upgrade .net framework and devexpress

--- bymodule/1/12/DataAccessScenarios/EFModel11.Designer.cs
+++ bymodule/1/12/DataAccessScenarios/EFModel11.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\git\class-winforms-online\samples\bymodule\1\12\DataAccessScenarios\EFModel1.edmx'. 
+﻿// T4 code generation is enabled for model 'D:\github\winforms-training-samples\bymodule\1\12\DataAccessScenarios\EFModel1.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
